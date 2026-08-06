8172243ddb5a0fa6d021be2f0e837053133b7be2
haha1590 <hami1683@gmail.com>
2026-08-06 20:02:01 +0900

    Add files via upload

---
+++ topics_r12.js
@@ -0,0 +1,23 @@
+/* 간호국가시험 모의고사 12회 - 문항별 주제 태그 (최다오답 심화해설 연결용) */
+(function(){
+  QUESTION_TOPIC[12] = {
+    1:"job_ethics_law", 2:"job_ethics_law", 3:"ward_safety_environment", 4:"basic_nursing_infection_control", 5:"nutrition_metabolism", 6:"skeletal_muscle_blood", 
+    7:"drug_medication_admin", 8:"bedmaking_hygiene", 9:"nutrition_metabolism", 10:"nutrition_metabolism", 11:"dental_oral_care", 12:"dental_oral_care", 
+    13:"oriental_medicine", 14:"oriental_medicine", 15:"mental_health_care", 16:"ward_safety_environment", 17:"general_review", 18:"drug_medication_admin", 
+    19:"msk_arthritis_care", 20:"consciousness_level", 21:"general_review", 22:"general_review", 23:"obstetric_emergency", 24:"obstetric_emergency", 25:"obstetric_emergency", 
+    26:"child_infection_dehydration", 27:"newborn_assessment", 28:"newborn_assessment", 29:"child_infection_dehydration", 30:"msk_arthritis_care", 31:"elderly_health_communication", 
+    32:"elderly_health_communication", 33:"burn_heat_cold_firstaid", 34:"cpr_emergency", 35:"burn_heat_cold_firstaid", 36:"health_education", 37:"health_education", 
+    38:"health_education", 39:"health_education", 40:"ltci", 41:"health_administration_system", 42:"health_administration_system", 43:"health_administration_system", 
+    44:"health_administration_system", 45:"ltci", 46:"environmental_health", 47:"environmental_health", 48:"nutrition_metabolism", 49:"environmental_health", 
+    50:"occupational_health", 51:"infectious_disease_grade", 52:"disease_prevention_levels", 53:"disease_prevention_levels", 54:"infectious_disease_general", 
+    55:"child_infection_dehydration", 56:"population_health_index", 57:"population_health_index", 58:"maternal_child_health", 59:"maternal_child_health", 
+    60:"defense_mechanism", 61:"community_visiting", 62:"ltci", 63:"ltci", 64:"mental_health_care", 65:"medical_law_records", 66:"mental_health_law", 67:"tuberculosis", 
+    68:"oral_health_law", 69:"blood_management_law", 70:"infectious_disease_grade", 71:"vital_signs_assessment", 72:"vital_signs_assessment", 73:"bedmaking_hygiene", 
+    74:"dysphagia_feeding", 75:"catheter_care", 76:"elimination_care", 77:"elimination_care", 78:"catheter_care", 79:"basic_nursing_infection_control", 
+    80:"basic_nursing_infection_control", 81:"basic_nursing_infection_control", 82:"basic_nursing_infection_control", 83:"basic_nursing_infection_control", 
+    84:"wound_dressing_sterile", 85:"wound_dressing_sterile", 86:"bedmaking_hygiene", 87:"bedmaking_hygiene", 88:"dental_oral_care", 89:"restraint_positioning", 
+    90:"restraint_positioning", 91:"restraint_positioning", 92:"restraint_positioning", 93:"restraint_positioning", 94:"restraint_positioning", 95:"burn_heat_cold_firstaid", 
+    96:"perioperative_care", 97:"lab_specimen", 98:"lab_specimen", 99:"lab_specimen", 100:"cpr_emergency", 101:"respiratory_care", 102:"oxygen_therapy", 
+    103:"oral_medication_practice", 104:"mental_health_care", 105:"hospice_endoflife"
+  };
+})();
